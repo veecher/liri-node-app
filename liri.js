4302d2a79801e68921cad5d95556e2a5897ce81a
richard baylis <veecher@gmail.com>
2018-12-15 17:40:54 -0800

    mostly working

--- liri.js
+++ liri.js
@@ -1,28 +1,91 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var request = require('request');
+var fs = require("fs");
+var command = process.argv[2];
 
-// var Spotify = require('node-spotify-api');
-// var spotify = new Spotify(keys.spotify);
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 
-// * `concert-this`
+// format concert times
+// do-what-it-says
 
-// * `spotify-this-song`
+function spotifySearch() {
+    var name = process.argv.slice(3).join(" ");
+    spotify.search({
+        type: 'track', query: name
+    },
+        function (err, body) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            else {
+                var returnData = body.tracks.items[0];
+                var songData = [
+                    ("artist: " + returnData.artists[0].name),
+                    ("song title: " + returnData.name),
+                    ("link: " + returnData.external_urls.spotify),
+                    ("album: " + returnData.album.name)
+                ].join("\n");
+                fs.appendFile("log.txt", songData + "\n\n\n")
+                console.log(songData);
+            };
+        });
+}
 
-// * `movie-this`
+function concertSearch() {
+    var artist = process.argv.slice(3).join(" ");
+    var URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    request(URL, function (err, response, body) {
+        var concertData = JSON.parse(body);
+        var logData = [
+            ("venue: " + concertData[0].venue.name),
+            ("city: " + concertData[0].venue.city + ", " + concertData[0].venue.region),
+            ("date: " + concertData[0].datetime)
+        ].join("\n");
+        fs.appendFile("log.txt", logData + "\n\n\n")
+        console.log(logData);
+    });
+}
 
-// * `do-what-it-says`
+function movieSearch() {
+    var title = process.argv.slice(3).join(" ");
+    var URL = "http://www.omdbapi.com/?apikey=trilogy&t=" + title;
+    request(URL, function (err, response, body) {
+        var movieData = JSON.parse(body);
+        var logData = [
+            ("title: " + movieData.Title),
+            ("release year: " + movieData.Year),
+            ("imdb rating: " + movieData.imdbRating),
+            ("rotten tomato score: " + movieData.Ratings[1].Value),
+            ("produced in: " + movieData.Country),
+            ("language: " + movieData.Language),
+            ("plot: " + movieData.Plot),
+            ("actors: " + movieData.Actors)
+        ].join("\n");
+        fs.appendFile("log.txt", logData + "\n\n\n")
+        console.log(logData);
+    });
+}
 
-// bandsintown
+if (command === "spotify-this-song"); {
+    spotifySearch();
+};
 
-// * `concert-this`
+if (command === "concert-this") {
+    concertSearch()
+};
 
-// node liri.js concert-this <artist/band name here>`
+if (command === "movie-this") {
+    movieSearch();
+};
 
-//    * This will search the Bands in Town Artist Events API (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`) for an artist and render the following information about each event to the terminal:
+// if (command === "do-what-it-says") {
+//     var searchArr = require("./random.txt").split(",");
+//     var searchType = searchArr[0];
+//     var searchTerm = searchArr[1].join(" ");
 
-//      * Name of the venue
-
-//      * Venue location
-
-//      * Date of the Event (use moment to format this as "MM/DD/YYYY")
+//     if (searchType === "spotify-this-song") {
+//         spotifySearch(searchTerm)
+//     };
+// };
